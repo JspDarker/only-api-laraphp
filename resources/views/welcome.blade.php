fe334b52a5a17d4a1e6d9937e13e278b1fd4d563
JsCore <thanhfgit@gmail.com>
2019-01-19 03:41:40 -0500

    config all branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,16 +81,16 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel @Api
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">Products</a>
+                    <a href="#">About</a>
+                    <a href="#">Services</a>
+                    <a href="#">Data</a>
+                    <a href="#">Api</a>
+                    <a href="#">String</a>
                 </div>
             </div>
         </div>
